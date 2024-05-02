e44907dd6a135a16fcfb3475899943c21e323d3e
KeerthikaReddyP <keerthikareddy267@gmail.com>
2024-05-02 22:51:36 +0530

    Dropdown edited

--- src/Header.js
+++ src/Header.js
@@ -15,6 +15,7 @@ function Header() {
         <button onClick={toggleDropdown} className="dropdown-btn">{selectedOption} ⌵</button>
         {isOpen && (
             <div className="dropdown-menu">
+              {console.log("heyyyy")}
                 <a href="/" onClick={()=>dropdownHandler("Patients")}>Patients</a>
                 <a href="/" onClick={()=>dropdownHandler("Consultants")}>Consultants</a>
                 <a href="/" onClick={()=>dropdownHandler("Others")}>Others</a>
